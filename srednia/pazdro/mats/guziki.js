1d552420ecc49450cb058a041406126d78078f43
Marek Kalus <marektg2@gmail.com>
2022-03-02 21:38:14 +0100

    update js file

--- srednia/pazdro/mats/guziki.js
+++ srednia/pazdro/mats/guziki.js
@@ -1,20 +1,20 @@
 function pokaz($a) {
     var z = $a;
     console.log(z);
-    var did = document.getElementById("r-"+z);
+    var did = document.getElementById(`r-${z}`);
     var rid = document.createElement("img");
-    rid.setAttribute('src', "roz/"+z+".png");
+    rid.setAttribute('src', `roz/${z}.png`);
     rid.setAttribute('class', 'img-fluid');
     did.appendChild(rid);
-    var uid = document.getElementById("g"+z);
+    var uid = document.getElementById(`g${z}`);
     uid.innerHTML = "Ukryj rozwiązanie";
-    uid.setAttribute("onclick", "usun('"+z+"')");
+    uid.setAttribute("onclick", `usun('${z}')`);
 }
 function usun($a) {
     var z = $a;
-    var did = document.getElementById("r-" + z);
+    var did = document.getElementById(`r-${z}`);
     did.innerHTML = null;
-    var uida = document.getElementById("g" + z);
+    var uida = document.getElementById(`g${z}`);
     uida.innerHTML = "Rozwiązanie";
-    uida.setAttribute("onclick", "pokaz('"+z+"')");
+    uida.setAttribute("onclick", `pokaz('${z}')`);
 }
